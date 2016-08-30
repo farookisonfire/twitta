9490246219e25a880fd991c0d7881c2e492d87c1
Farook Khan <farook.khan@gmail.com>
2016-08-30 10:40:58 -0700

    Issue #2 -- adding follow button and functionality

--- default.js
+++ default.js
@@ -10,7 +10,9 @@ for ( var i = 0 ; i < tweets.length ; i++) {
   var nameSpan = document.createElement('span');
   var usernameSpan = document.createElement('span');
   var dateSpan = document.createElement('span');
-  var contentDiv = document.createElement('div')
+  var contentDiv = document.createElement('div');
+	var button = document.createElement('button');
+	var following = [];
 
   tweetDiv.id = 'tweet-div';
   picDiv.id = 'pic-div';
@@ -21,12 +23,13 @@ for ( var i = 0 ; i < tweets.length ; i++) {
   dateSpan.id = 'date';
   contentDiv.id = 'content';
 
-
   picture.src= tweets[i].pic;
   nameSpan.textContent = tweets[i].name;
 	usernameSpan.textContent = tweets[i].username;
 	dateSpan.textContent = tweets[i].date;
 	contentDiv.textContent = tweets[i].content;
+	button.textContent = 'Follow ' + tweets[i].name;
+
   contentDiv.classList.add('content');
   // nameSpan.classList.add('panel-heading');
   // usernameSpan.classList.add('panel-heading');
@@ -35,9 +38,13 @@ for ( var i = 0 ; i < tweets.length ; i++) {
   picDiv.classList.add('col-md-2');
   spanDiv.classList.add('col-md-10');
   contentDiv.classList.add('col-md-10');
-  tweetDiv.classList.add('col-md-12')
+  tweetDiv.classList.add('col-md-12');
+	button.classList.add('btn');
+	button.classList.add('follow');
+	button.setAttribute('name', tweets[i].name);
+	button.setAttribute('id', tweets[i].id);
 
-  tweetDiv.classList.add('panel');
+	tweetDiv.classList.add('panel');
   // tweetDiv.classList.add('panel-default');
 
   picDiv.appendChild(picture);
@@ -48,7 +55,19 @@ for ( var i = 0 ; i < tweets.length ; i++) {
   tweetDiv.appendChild(picDiv);
   tweetDiv.appendChild(spanDiv);
 	tweetDiv.appendChild(contentDiv);
+	tweetDiv.appendChild(button);
 
   theTweets.appendChild(tweetDiv);
-
 }
+
+// document.body.addEventListener('click', function(event){
+// 	if(event.target.className.indexOf('follow') !== -1) {
+// 		var name = event.target.getAttribute('name');
+// 		var id = event.target.getAttribute('id');
+// 		for (i = 0 ; i < following.length ; i++) {
+// 			if (following[i].indexOf(id) === -1){
+// 				following.push([name,id])
+// 				console.log('following');
+// 	}}}
+//
+// });
