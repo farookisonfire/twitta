6cc142fb9e9cd9f1ec103720008c54adda0c8d2c
Farook Khan <farook.khan@gmail.com>
2016-09-01 15:38:26 -0700

    Issue #3 - user can post to their timeline

--- default.js
+++ default.js
@@ -4,6 +4,10 @@ var following =[]
 var profileCard = document.getElementById('profile-card')
 var yourTweet = document.getElementById('your-tweet');
 var tweetButton = document.getElementById('tweet-button');
+var tweetCount = 0;
+var followingCount = 0;
+var userId = '695412312234234213412312545623';
+
 
 for ( var i = 0 ; i < tweets.length ; i++) {
 	var tweetDiv = document.createElement('div');
@@ -54,7 +58,7 @@ for ( var i = 0 ; i < tweets.length ; i++) {
 	button.classList.add('follow');
 	button.setAttribute('name', tweets[i].name);
 	button.setAttribute('id', tweets[i].id);
-
+	button.setAttribute('content', tweets[i].content);
 	tweetDiv.classList.add('panel');
   tweetDiv.classList.add('panel-default');
 
@@ -75,19 +79,26 @@ document.body.addEventListener('click', function(event){
 	clear(followers);
 
 	if(event.target.className.indexOf('follow') !== -1) {
+		var followingCounter = document.getElementById('following-counter');
 		var name = event.target.getAttribute('name');
 		var id = event.target.getAttribute('id');
+		var content = event.target.getAttribute('content');
+		event.target.classList.add('now-following')
+		event.target.textContent = 'Following!'
 		if (typeof following === 'undefined') {
 		  // following = [];
-			following.push([name, id]);
+			following.push([name, id, content]);
+			followingCount += 1;
+			followingCounter.textContent = followingCount;
 			console.log(following);
 		} // end of if conditional
 		else if (check(following, id) === 0){
-				following.push([name,id])
+				followingCount += 1;
+				followingCounter.textContent = followingCount;
+				following.push([name, id, content])
 				console.log(following);
 			} // end of else if conditional
 	} // end of first if conditional
-
 	listFollower(following);
 }); // end of event listener
 
@@ -115,11 +126,13 @@ function listFollower(array) {
 	followers.appendChild(followingDiv);
 
 	for (var i = 0 ; i < array.length ; i++) {
-		var follower = array[i][0];
-		var followerP = document.createElement('p');
-		followerP.textContent = follower;
-		followerP.classList.add('panel-body');
-		followers.appendChild(followerP);
+		if (array[i][1] !== '695412312234234213412312545623'){
+			var follower = array[i][0];
+			var followerP = document.createElement('p');
+			followerP.textContent = follower;
+			followerP.classList.add('panel-body');
+			followers.appendChild(followerP);
+		}
 	}
 }
 
@@ -137,57 +150,62 @@ var followTweets = document.getElementById('follow-tweets')
 home.addEventListener('click', function(){
 	clear(followTweets);
 	showIt(profileCard);
-	theTweets.classList.remove('live')
-	theTweets.classList.add('hide');
-	followTweets.classList.remove('hide');
-	followTweets.classList.add('live')
+	// theTweets.classList.remove('live')
+	hideIt(theTweets);
+	showIt(followTweets);
+	compareTweetsToFollowing(tweets, following);
 
-	for (var i = 0 ; i < tweets.length ; i++) {
-		for (var y = 0 ; y < following.length ; y++) {
-			if(tweets[i].id == following[y][1]) {
-				var followTweetDiv = document.createElement('div');
-				var followPicDiv = document.createElement('div');
-				var followSpanDiv = document.createElement('div');
-				var followContent = document.createElement('div');
+	var followButton = document.getElementsByClassName('follow');
+	for (i = 0 ; i < followButton.length ; i++) {
+		hideIt(followButton[i]);
+	}
 
-				var followPicture = document.createElement('img');
-				var followNameSpan = document.createElement('span');
-				var followUserNameSpan = document.createElement('span');
-				var followDateSpan = document.createElement('span');
-
-				followPicture.src = tweets[i].pic;
-				followPicture.classList.add('wall-pic')
-				followPicDiv.appendChild(followPicture);
-				followNameSpan.textContent = tweets[i].name;
-				followUserNameSpan.textContent = tweets[i].username;
-				followDateSpan.textContent = tweets[i].date;
-
-				followNameSpan.classList.add('span-name');
-				followUserNameSpan.classList.add('span-user-name');
-				followDateSpan.classList.add('span-date');
-
-				followSpanDiv.appendChild(followNameSpan);
-				followSpanDiv.appendChild(followUserNameSpan);
-				followSpanDiv.appendChild(followDateSpan);
-				followContent.textContent = tweets[i].content;
-
-				followTweetDiv.classList.add('col-md-12');
-				followPicDiv.classList.add('col-md-2');
-				followSpanDiv.classList.add('col-md-10');
-				followContent.classList.add('col-md-10');
-				followTweetDiv.classList.add('panel');
-				followTweetDiv.classList.add('panel-default');
-				followContent.classList.add('panel-body')
-				followTweetDiv.classList.add('follow-tweet-div');
-				followTweetDiv.appendChild(followPicDiv);
-				followTweetDiv.appendChild(followSpanDiv);
-				followTweetDiv.appendChild(followContent);
-
-				followTweets.appendChild(followTweetDiv);
-			}
-
-		} // end of inner for loop
-	} // end of outer for loop
+	// for (var i = 0 ; i < tweets.length ; i++) {
+	// 	for (var y = 0 ; y < following.length ; y++) {
+	// 		if(tweets[i].id === following[y][1] && tweets) {
+	// 			var followTweetDiv = document.createElement('div');
+	// 			var followPicDiv = document.createElement('div');
+	// 			var followSpanDiv = document.createElement('div');
+	// 			var followContent = document.createElement('div');
+	//
+	// 			var followPicture = document.createElement('img');
+	// 			var followNameSpan = document.createElement('span');
+	// 			var followUserNameSpan = document.createElement('span');
+	// 			var followDateSpan = document.createElement('span');
+	//
+	// 			followPicture.src = tweets[i].pic;
+	// 			followPicture.classList.add('wall-pic')
+	// 			followPicDiv.appendChild(followPicture);
+	// 			followNameSpan.textContent = tweets[i].name;
+	// 			followUserNameSpan.textContent = tweets[i].username;
+	// 			followDateSpan.textContent = tweets[i].date;
+	//
+	// 			followNameSpan.classList.add('span-name');
+	// 			followUserNameSpan.classList.add('span-user-name');
+	// 			followDateSpan.classList.add('span-date');
+	//
+	// 			followSpanDiv.appendChild(followNameSpan);
+	// 			followSpanDiv.appendChild(followUserNameSpan);
+	// 			followSpanDiv.appendChild(followDateSpan);
+	// 			followContent.textContent = tweets[i].content;
+	//
+	// 			followTweetDiv.classList.add('col-md-12');
+	// 			followPicDiv.classList.add('col-md-2');
+	// 			followSpanDiv.classList.add('col-md-10');
+	// 			followContent.classList.add('col-md-10');
+	// 			followTweetDiv.classList.add('panel');
+	// 			followTweetDiv.classList.add('panel-default');
+	// 			followContent.classList.add('panel-body')
+	// 			followTweetDiv.classList.add('follow-tweet-div');
+	// 			followTweetDiv.appendChild(followPicDiv);
+	// 			followTweetDiv.appendChild(followSpanDiv);
+	// 			followTweetDiv.appendChild(followContent);
+	//
+	// 			followTweets.appendChild(followTweetDiv);
+	// 		}
+	//
+	// 	} // end of inner for loop
+	// } // end of outer for loop
 
 }) // end of event listener
 
@@ -195,6 +213,10 @@ var bird = document.getElementById('logo');
 
 bird.addEventListener('click', function(){
 	hideIt(profileCard);
+	var followButton = document.getElementsByClassName('follow');
+	for (i = 0 ; i < followButton.length ; i++) {
+		showIt(followButton[i]);
+	}
 	theTweets.classList.remove('hide')
 	theTweets.classList.add('live');
 	followTweets.classList.remove('live');
@@ -220,25 +242,31 @@ yourTweet.addEventListener('click', function(){
 })
 
 tweetButton.addEventListener('click', function() {
+	tweetCount += 1;
+	var tweetCounter = document.getElementById('tweet-counter');
+		tweetCounter.textContent = tweetCount;
 	var myTweet = {}
-		myTweet.id = '6954';
+		myTweet.id = '695412312234234213412312545623';
 		myTweet.name = 'name';
 		myTweet.username = 'username';
 		myTweet.date = Date();
 		myTweet.content = yourTweet.value;
-		myTweet.pic = 'dummy.png'
+		myTweet.pic = 'dummy.png';
 
 	tweets.unshift(myTweet);
+	following.unshift([myTweet.name, myTweet.id, myTweet.content]);
 	clear(theTweets);
+	clear(followTweets);
 	for (var i = 0 ; i < tweets.length ; i++) {
-	build(tweets[i]);
+		buildTweets(tweets[i]);
 	}
 
+	compareTweetsToFollowing(tweets, following);
 }); //end of event listener
 
 
 
-function build(tweet) {
+function buildTweets(tweet) {
 	var buildTweetDiv = document.createElement('div');
 	buildTweetDiv.classList.add('col-md-12');
 	buildTweetDiv.classList.add('panel');
@@ -272,10 +300,98 @@ function build(tweet) {
 	buildContent.classList.add('col-md-10');
 	buildContent.textContent = tweet.content;
 
+
+	var buildButton = document.createElement('button');
+	buildButton.textContent = 'Follow';
+	buildButton.classList.add('btn');
+	buildButton.classList.add('button');
+	buildButton.classList.add('follow');
+	buildButton.setAttribute('name', tweet.name);
+	buildButton.setAttribute('id', tweet.id);
+	buildButton.setAttribute('content', tweet.content);
+
 	buildTweetDiv.appendChild(buildPicDiv);
 	buildTweetDiv.appendChild(buildSpanDiv);
 	buildTweetDiv.appendChild(buildContent);
+	buildTweetDiv.appendChild(buildButton);
 	buildTweetDiv.id = "tweet-div";
 
 	theTweets.appendChild(buildTweetDiv);
+}  // END OF BUILD FUNCTION, WHICH BUILDS TWEETS AND APPENDS
+
+function buildFollowTweets(tweet) {
+	var buildTweetDiv = document.createElement('div');
+	buildTweetDiv.classList.add('col-md-12');
+	buildTweetDiv.classList.add('panel');
+	buildTweetDiv.classList.add('panel-default');
+
+	var buildPicDiv = document.createElement('div');
+	buildPicDiv.classList.add('col-md-2');
+	var buildPicture = document.createElement('img');
+	buildPicture.src = tweet.pic;
+	buildPicture.classList = 'wall-pic';
+	buildPicDiv.appendChild(buildPicture);
+
+	var buildSpanDiv = document.createElement('div');
+	buildSpanDiv.classList.add('col-md-10');
+	var buildSpanName = document.createElement('span');
+	buildSpanName.textContent = tweet.name;
+	buildSpanName.classList.add('span-name');
+	var buildSpanUserName = document.createElement('span');
+	buildSpanUserName.textContent = tweet.username
+	buildSpanName.classList.add('span-user-name');
+	var buildSpanDate = document.createElement('span');
+	buildSpanDate.textContent = tweet.date;
+	buildSpanName.classList.add('span-date');
+	buildSpanDiv.appendChild(buildSpanName);
+	buildSpanDiv.appendChild(buildSpanUserName);
+	buildSpanDiv.appendChild(buildSpanDate);
+
+	var buildContent = document.createElement('div');
+	buildContent.classList.add('content');
+	buildContent.classList.add('panel-body');
+	buildContent.classList.add('col-md-10');
+	buildContent.textContent = tweet.content;
+
+
+	var buildButton = document.createElement('button');
+	buildButton.textContent = 'Follow';
+	buildButton.classList.add('btn');
+	buildButton.classList.add('button');
+	buildButton.classList.add('follow');
+	buildButton.setAttribute('name', tweet.name);
+	buildButton.setAttribute('id', tweet.id);
+
+	buildTweetDiv.appendChild(buildPicDiv);
+	buildTweetDiv.appendChild(buildSpanDiv);
+	buildTweetDiv.appendChild(buildContent);
+	buildTweetDiv.appendChild(buildButton);
+	buildTweetDiv.id = "tweet-div";
+
+	followTweets.appendChild(buildTweetDiv);
+}  // END OF BUILD FUNCTION, WHICH BUILDS TWEETS AND APPENDS
+
+function compareTweetsToFollowing(theCollection, theFollowing) {
+	for (var i = 0 ; i < theCollection.length ; i++) {
+		for (var y = 0 ; y < theFollowing.length ; y++) {
+			if (theCollection[i].id === theFollowing[y][1] && theCollection[i].content === theFollowing[y][2]) {
+				buildFollowTweets(theCollection[i]);
+			}
+		}
+	}
 }
+
+var seeMyTweets = document.getElementById('tweet-count-link');
+seeMyTweets.addEventListener('click', function(){
+clear(followTweets);
+	for (var i = 0 ; i < tweets.length ; i++) {
+		if (tweets[i].id === userId) {
+			buildFollowTweets(tweets[i]);
+		}
+	}
+
+	var followButton = document.getElementsByClassName('follow');
+	for (i = 0 ; i < followButton.length ; i++) {
+		hideIt(followButton[i]);
+	}
+})
